bc06d1b426ca1146830f068aead5cebeb2084533
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-03-19 22:53:24 -0700

    added ReadMe notes

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Challenge5-WorkdayScheduler
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+
+## Installation
+
+N/A
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+I'd like to credit my friends Ryan and Zack who are currently web developers in San Diego, CA and Los Angeles, CA.  They each were able to help me walk through the step-by-step of coming up with my code as well as testing to make sure it ran correctly.  
+
+## License
+
+N/A
+
+
+## Features
+
+
+
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 const workHours = [
 "9:00 AM",
 "10:00 AM",
@@ -17,6 +14,10 @@ const workHoursNum = [
   9, 10, 11, 12, 13, 14, 15, 16, 17
 ];
 
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+
 $(function () {
 
   // Added code to display the current date in the header of the page.
@@ -30,6 +31,9 @@ $(function () {
 
   var timeBlocksEl= $('#timeBlocks');
 
+   // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
+  // HINTS: How can the id attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
 
 
 for (var i=0; i< workHours.length; i++) {
@@ -55,6 +59,11 @@ else {hourBlock.addClass("future");}
   hourBlock.append(saveBtn);
   hourDiv.text(workHours[i]);
   saveBtn.append(saveIcon);
+
+  var userInput = ("");
+  localStorage.setItem(userInput, "");
+
+
 }
 
 
@@ -72,11 +81,7 @@ else {hourBlock.addClass("future");}
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. 
-  // HINTS: How can the id attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+ 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. 
