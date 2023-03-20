ab4e8124510ebb48f6643b5f52b1383912473fec
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-03-20 16:52:35 -0700

    tested code, ready for deploy

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const workHours = [
-"9:00 AM",
-"10:00 AM",
-"11:00 AM",
-"12:00 PM",
-"1:00 PM",
-"2:00 PM",
-"3:00 PM",
-"4:00 PM",
-"5:00 PM"
+  "9:00 AM",
+  "10:00 AM",
+  "11:00 AM",
+  "12:00 PM",
+  "1:00 PM",
+  "2:00 PM",
+  "3:00 PM",
+  "4:00 PM",
+  "5:00 PM"
 ];
 
 const workHoursNum = [
@@ -20,100 +20,79 @@ const workHoursNum = [
 
 $(function () {
 
-  // Added code to display the current date in the header of the page.
-
+  // Added code to display the current date in the header of the page using dayjs.
   var currentDate = dayjs();
   $('#currentDay').text(currentDate.format('dddd, MMMM DD, YYYY hh:mm A'));
 
-  // var currentHour = dayjs().format("H");
-  var currentHour = 10;
+  var currentHour = dayjs().format("H");
   console.log(currentHour);
 
-  var timeBlocksEl= $('#timeBlocks');
-  
 
-   // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
+  var timeBlocksEl = $('#timeBlocks');
+
+
+  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
   // HINTS: How can the id attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
 
 
-for (var i=0; i< workHours.length; i++) {
-  var hourBlock = $('<div class ="row time-block">');
-  hourBlock.addClass("");
+  for (var i = 0; i < workHours.length; i++) {
+    // hourBlock is the entire row 
+    var hourBlock = $('<div class ="row time-block">');
 
-if (workHoursNum [i]< currentHour) {
-  hourBlock.addClass("past");
-}
-else if (workHoursNum [i] == currentHour) {
-  hourBlock.addClass("present");
-}
-else {hourBlock.addClass("future");}
+    if (workHoursNum[i] < currentHour) {
+      hourBlock.addClass("past");
+    }
+    else if (workHoursNum[i] == currentHour) {
+      hourBlock.addClass("present");
+    }
+    else { hourBlock.addClass("future"); }
+
+    // hourDiv is the hour for the row from 9:00 AM - 5:00 PM
+    var hourDiv = $('<div class="col-2 col-md-1 hour text-center py-3">');
 
 
-  var hourDiv = $('<div class="col-2 col-md-1 hour text-center py-3">');
-  var task = $('<textarea class= "col-8 col-md-10 description" rows="3">');
-  var saveBtn = $('<button class = "btn saveBtn col-2 col-md-1" aria-label="save">');
-  var saveIcon = $('<i class="fas fa-save" aria-hidden="true">');
-  timeBlocksEl.append(hourBlock);
-  hourBlock.append(hourDiv);
-  hourBlock.append(task);
-  hourBlock.append(saveBtn);
-  hourDiv.text(workHours[i]);
-  saveBtn.append(saveIcon);
+    // task is the middlearea where userInput will be 
+    var task = $('<textarea class= "col-8 col-md-10 description" rows="3">');
 
-  // saveBtn.on("click", function (event) {
-  //   event.preventDefault();
-  
-  
+    // get what's in local storage and check if its empty or not
+    var storedData = localStorage.getItem(workHours[i]);
+    if (storedData != null) {
+      task.val(storedData);
+    }
 
-  //   var userInput = "";
-  //   localStorage.setItem("task", userInput);
-  
-  //   // task (userInput)
-  //   // userInput written to the task var to be saved in local storage
-  //   });
-    
+
+
+    var saveBtn = $('<button class = "btn saveBtn col-2 col-md-1" aria-label="save">');
+    var saveIcon = $('<i class="fas fa-save" aria-hidden="true">');
+    // appended all new elements created in js to page 
+    timeBlocksEl.append(hourBlock);
+    hourBlock.append(hourDiv);
+    hourBlock.append(task);
+    hourBlock.append(saveBtn);
+    hourDiv.text(workHours[i]);
+    saveBtn.append(saveIcon);
 
   }
-  
+  // jQuery selector for all rows 
   var timeBlockRow = $('.time-block');
+
+  //  select a save button in the clicked row 
+  // the "click" is being assigned to the class .time-block which is any row but is only listening for the 'saveBtn' click 
 
   timeBlockRow.on("click", ".saveBtn", function (event) {
     event.preventDefault();
-  
-    console.log($( this ).parent().children().eq(0).text());
 
-    var taskRow = $( this ).parent().children().eq(0).text();
 
-    var userInput = "";
+    console.log($(this).parent().children().eq(1).val());
+
+    // (this) points to the save button
+    var taskRow = $(this).parent().children().eq(0).text();
+
+    var userInput = $(this).parent().children().eq(1).val();
     localStorage.setItem(taskRow, userInput);
-  
-  
-    // userInput written to the task var to be saved in local storage
-    });
-    
-
-  
 
 
-	
+  });
 
- 
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. 
-  // HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
- 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. 
-  // HINT: How can the id attribute of each time-block be used to do this?
-  //
- 
 });
